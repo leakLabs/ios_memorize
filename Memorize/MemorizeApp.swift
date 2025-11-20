6c03a7c13001cd7d225f24d6a2fc67339da601fb
leakLabs <oko48ru@mail.ru>
2025-11-20 22:24:46 +0300

    initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by User on 20.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
